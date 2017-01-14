3729fe017fa2bbcc655d358cace1c141c572e44e
lufy <lufy90@qq.com>
2017-01-14 23:48:03 +0800

    Add posts view and detail view, and link them on the page. Little change on derectory structure.

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'home'
 urlpatterns =[
   url(r'^$', views.HomeView.as_view(), name='home'),
+  url(r'^posts/$', views.PostsView.as_view(), name='posts'),
+  url(r'^posts/(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
 ]
--- home/views.py
+++ home/views.py
@@ -4,9 +4,21 @@ from django.views import generic
 from django.http import HttpResponse
 from django.template import loader
 
+from .models import Post
+
 class HomeView(generic.TemplateView):
   template_name = 'home/home.html'
 
+class PostsView(generic.ListView):
+  template_name = 'home/posts.html'
+  context_object_name = 'posts'
+  def get_queryset(self):
+    """ Return all of the posts """
+    return Post.objects.order_by('-last_modified')
+
+class DetailView(generic.DetailView):
+  model = Post
+  template_name = 'home/detail.html'
 #def home(request):
 #    template = loader.get_template('home/home.html')
 #    return HttpResponse(template.render(request))
