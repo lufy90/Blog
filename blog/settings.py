e836b76b81b09bbd4d7382fca7e1911a827dbe32
lufei <very90@foxmail.com>
2017-08-23 17:48:30 +0800

    Deployment with uwsgi and nginx

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'n*=w!@5c6*x#8v^j46c!v0$&^d0vo%t2it3hune3873&$i%of6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [u'192.168.88.150', u'linux', u'127.0.0.1']
+ALLOWED_HOSTS = [u'test', u'127.0.0.1']
 
 
 # Application definition
