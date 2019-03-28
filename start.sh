efcab621d0bff5e89d808c1db9874b78ac8cc015
lufei <very90@foxmail.com>
2019-03-28 12:36:39 +0800

    Add instruction in comment.

--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+# Make a schedule with 'crontab -e':
+# 10 2 * * * cd /home/sb/Blog/; ./start.sh
+
+
+for p in `pgrep uwsgi`
+do
+kill -9 $p
+done
+
 DATE=`date +%Y%m%d`
-uwsgi --ini blog.ini --daemonize /tmp/uwsgi-$DATE.log
+uwsgi --ini blog.ini --daemonize $PWD/log/uwsgi-${DATE}.log
