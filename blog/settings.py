9628e846c817e458b973121b918c89002d593770
lufei <very90@foxmail.com>
2019-03-28 12:38:51 +0800

    Change setting only to fit my local host.

--- blog/settings.py
+++ blog/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'n*=w!@5c6*x#8v^j46c!v0$&^d0vo%t2it3hune3873&$i%of6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-ALLOWED_HOSTS = [u'linux',]
+DEBUG = True
+ALLOWED_HOSTS = [u'test',]
 
 
 # Application definition
@@ -125,9 +125,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-#ESATIC_ROOT = os.path.join(BASE_DIR, "static/")
+#SATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static/"),
+]
 #TINYMCE_DEFAULT_CONFIG = {
 #    'theme': "advanced",
 #    'theme_advanced_buttons4': "code",
--- test.py
+++
@@ -1,4 +0,0 @@
-# for test.
-def application(env, start_response):
-  start_response('200 OK', [('Content-Type','test/html')])
-  return ['Hello world']
